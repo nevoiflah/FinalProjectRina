676ef86aab75c2cabd12a3d2b7576970a1987372
NevoIflah <nevo.iflah6@gmail.com>
2025-11-14 22:08:59 +0200

    improved UI and layout

--- Server/obj/Debug/net8.0/FinalProjectRina.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/FinalProjectRina.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProjectRina.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e80881b4ef2f02cc8b9d48ff5c8d4da328ba928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e791be2ece98e504a676e74c485ce3f5967e8c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProjectRina.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProjectRina.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
